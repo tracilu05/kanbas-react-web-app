07a7ecab03abb62d35c7117a40e0eff0a561c777
tracilu05 <tracilu341@gmail.com>
2024-09-10 20:42:19 -0400

    a1 html fa34

---
+++ src/AllLabs/index.tsx
@@ -0,0 +1,10 @@
+export default function Labs(){
+    return (
+        <div>
+        <h2>Lab 1</h2>
+        <h3> Exercise 1.1</h3>
+        <h4> Exercise 1.2</h4>  
+        <h4> Exercise 1.3</h4> 
+        </div>
+    );
+}
